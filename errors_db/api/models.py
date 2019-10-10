d5945614912f29e021eb99699e9b202bb4871baa
Brandon Parker <brandon.parker@salesforce.com>
2019-10-10 15:53:05 -0700

    Message Model

--- errors_db/api/models.py
+++ errors_db/api/models.py
@@ -1,6 +1,10 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager as BaseUserManager
 from model_utils import Choices
+
+from enum import Enum
 
 from . import model_mixins as mixins
 
@@ -11,4 +15,17 @@ class UserManager(BaseUserManager):
 
 class User(mixins.HashIdMixin, AbstractUser):
     objects = UserManager()
+
+
+class ErrorMessage(models.Model):
+    message = models.TextField()
+
+
+class ErrorInstance(models.Model):
+    context = JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    message = models.ForeignKey(ErrorMessage, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["created"]
 
