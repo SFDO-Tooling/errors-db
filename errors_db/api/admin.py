fc59a6e971a93008a12973acea32a53013e81524
Brandon Parker <brandon.parker@salesforce.com>
2019-10-10 15:52:51 -0700

    update admin.py with ErrorInstance

--- errors_db/api/admin.py
+++ errors_db/api/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
 from .models import User
+from errors_db.api.models import ErrorInstance
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "is_active", "is_staff", "is_superuser", "date_joined")
     search_fields = ("username",)
+
+
+@admin.register(ErrorInstance)
+class ErrorInstanceAdmin(admin.ModelAdmin):
+    list_display = ("message", "context", "created")
+    list_filter = ["created"]
+
