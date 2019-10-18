892f89563ce661d87d377dd35fc50d081efa9548
Brandon Parker <brandon.parker@salesforce.com>
2019-10-18 14:59:07 -0700

    add models to api ui

--- errors_db/api/admin.py
+++ errors_db/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import User
-from errors_db.api.models import ErrorInstance
+from errors_db.api.models import ErrorInstance, Situation, Solution
 
 
 @admin.register(User)
@@ -13,5 +13,17 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(ErrorInstance)
 class ErrorInstanceAdmin(admin.ModelAdmin):
     list_display = ("message", "context", "created")
+    list_filter = ["created"]
+
+
+@admin.register(Situation)
+class SituationAdmin(admin.ModelAdmin):
+    list_display = ("error_msg", "context", "created")
+    list_filter = ["created"]
+
+
+@admin.register(Solution)
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ("solution_type", "text", "situation", "created")
     list_filter = ["created"]
 
