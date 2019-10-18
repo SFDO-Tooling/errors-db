b9758f2da2a2b5fdcceda07756ceadc359d8183b
Brandon Parker <brandon.parker@salesforce.com>
2019-10-18 15:00:49 -0700

    test views.py

--- errors_db/api/tests/views.py
+++ errors_db/api/tests/views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from errors_db.api.models import ErrorInstance, Solution, Situation
+
 
 @pytest.mark.django_db
 def test_user_view(client):
@@ -8,3 +10,37 @@ def test_user_view(client):
 
     assert response.status_code == 200
     assert response.json()["username"].endswith("@example.com")
+
+
+@pytest.mark.django_db
+class TestErrorInstanceViewSet:
+    @pytest.fixture
+    def create_solution(self):
+        def _create_solution(type, text, situation=None):
+            Solution.objects.create(solution_type=type, text=text, situation=situation)
+
+        return _create_solution
+
+    @pytest.fixture
+    def create_situation(self):
+        def _create_situation(error_msg, context):
+            Situation.objects.create(error_msg=error_msg, context=context)
+
+        return _create_situation
+
+    def test_create(self, client, create_solution):
+        error_data = {
+            "message": "Test Message",
+            "context": {},
+            "stacktrace": "test stacktrace",
+        }
+
+        create_solution(0, "Please visit this doc: http://help.salesforce.com/")
+        create_solution(1, "Please retry the operation.")
+
+        response = client.post(
+            reverse("errorinstance-list"), data=error_data, format="json"
+        )
+
+        expected_data = {"solutions": ["solution1", "solution2"]}
+        assert response.status_code == 201
